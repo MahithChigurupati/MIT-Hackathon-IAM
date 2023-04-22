7c234b28240108170de0c34fe94a136d9786401f
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-04-21 23:42:51 -0400

    GMP & IND,CND and USA Govt Contracts

--- contracts/GeneralMessagePassing.sol
+++ contracts/GeneralMessagePassing.sol
@@ -18,6 +18,92 @@ interface IGovernment {
 
     function getCitizen(uint256 id) external view returns(citizen memory data);
 
+    function setAbroadCitizen(citizen memory datafromAbroad ) external view;
+
 }
 
-contract SenderReceiver is AxelarExecutable{}
+contract SenderReceiver is AxelarExecutable{
+    IAxelarGasService public immutable gasService;
+    IGovernment public government;
+    address localChainGovernmentContract;
+
+    constructor(address gateway_, address gasService_) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasService_);
+    }
+
+    function sendMessage(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        uint256 message_
+    ) external payable {
+        bytes memory payload = abi.encode(message_);
+        gasService.payNativeGasForContractCall{value: msg.value}(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            msg.sender
+        );
+
+        gateway.callContract(destinationChain, destinationAddress, payload);
+
+    }
+
+    function sendResponse(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        IGovernment.citizen memory message_
+    ) public payable {
+        bytes memory payload = abi.encode(message_);
+        gasService.payNativeGasForContractCall{value: msg.value}(
+            address(this),
+            destinationChain,
+            destinationAddress,
+            payload,
+            msg.sender
+        );
+
+        gateway.callContract(destinationChain, destinationAddress, payload);
+
+    }
+
+    function setGovernment(address addy) public {
+        localChainGovernmentContract = addy;
+    }
+
+    function _execute(
+        string calldata sourceChain,
+        string calldata sourceAddress,
+        bytes calldata payload_
+    ) internal override {
+
+        government = IGovernment(localChainGovernmentContract);
+
+        if(payload_.length > 0 && payload_[0] < 0x80){
+            uint256 message = abi.decode(payload_, (uint256));
+
+            IGovernment.citizen memory info = getDataFromGovernment(message);
+
+            sendResponse(sourceChain,sourceAddress,info);
+
+        }else{
+            IGovernment.citizen memory message = abi.decode(payload_, (IGovernment.citizen));
+            government.setAbroadCitizen(message);
+
+        }
+
+
+    }
+
+    function getDataFromGovernment(uint msge) public view returns(IGovernment.citizen memory data){
+
+        data = government.getCitizen(msge);
+        return data;
+    }
+}
+
+
+
+
+
+
--- contracts/IND_Government.sol
+++ contracts/IND_Government.sol
@@ -11,4 +11,66 @@ interface IGMP {
     ) external payable;
 }
 
-contract IND_Government{}
+contract IND_Government{
+
+    IGMP public sendReceiveMessage;
+    address localGMP;
+    uint256 UID = 0;
+    uint256 f_UID = 0;
+
+    struct governmentList{
+        string chain;
+        string governmentGMPContract;
+    }
+
+    struct citizen {
+        string name;
+        string dob;
+        string home;
+        uint ssn;
+        string passport;
+    }
+
+    mapping(uint => citizen) public localCitizens;
+    mapping(uint => citizen) public abroadCitizens;
+    mapping(string => governmentList) public internationalChains;
+
+    constructor(address GMP){
+        localGMP = GMP;
+        sendReceiveMessage = IGMP(localGMP);
+    }
+
+    function addCountry(string memory country, string memory countryChain, string memory countryGMPContract) public {
+        governmentList memory government = governmentList(countryChain,countryGMPContract);
+        internationalChains[country] = government;
+    }
+
+    function addCitizen(string memory name, string memory dob, string memory home, uint ssn, string memory passport) public {
+        UID++;
+        citizen memory newCitizen = citizen(name, dob, home, ssn, passport);
+        localCitizens[UID] = newCitizen;
+    }
+
+    function getCitizen(uint256 id) public view returns(citizen memory){
+        return localCitizens[id];
+    }
+
+    function getCitizenAbroad(uint256 citizenUid, string memory countryCode) public{
+        governmentList memory gov = internationalChains[countryCode];
+        string memory destinationChain = gov.chain;
+        string memory destinationAddress = gov.governmentGMPContract;
+
+        sendReceiveMessage.sendMessage(destinationChain, destinationAddress, citizenUid);
+    }
+
+    function setAbroadCitizen(citizen memory datafromAbroad ) public {
+        f_UID++;
+        abroadCitizens[f_UID] = datafromAbroad;
+    }
+}
+
+
+
+
+
+
